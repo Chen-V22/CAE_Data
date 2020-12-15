598459c3d9ef0b09f7d0ea6ac0a8d29e777b3745
tom <m4a1789456@gmail.com>
2020-12-15 16:08:38 +0800

    Add LogSearch_Model

---
+++ CAEProject/Models/LogSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CAEProject.Models
+{
+    public class LogSearch
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "編號")]
+        public int Id { get; set; }
+
+        [Display(Name = "事件類別")]
+        public string EventType { get; set; }
+
+        [Display(Name = "使用者")]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Display(Name = "事件日期")]
+        public DateTime EventDateTime { get; set; }
+
+        [Display(Name = "事件位置")]
+        public string EventUrl { get; set; }
+
+        [Display(Name = "事件內容")]
+        public string EventCount { get; set; }
+    }
+}
